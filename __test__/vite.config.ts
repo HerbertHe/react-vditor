21f571176b8a70c5c567d2449295f1cc38c75999
Herbert <herbert.he0229@gmail.com>
2021-08-07 10:34:41 +0800

    feat :sparkles: : add types & test

--- __test__/vite.config.ts
+++ __test__/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite"
+import reactRefresh from "@vitejs/plugin-react-refresh"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+    plugins: [reactRefresh()],
+    build: {
+        outDir: path.join(process.cwd(), "../docs"),
+    },
 })
